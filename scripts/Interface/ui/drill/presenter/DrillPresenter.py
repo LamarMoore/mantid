2bf39246018d7b9e0be14ac226748a487486ae0a
Guillaume Communie <communieg@ill.eu>
2021-03-11 09:44:24 +0100

    [DRILL] add filename in window title
    When loading a file, the filename is added to the title bar. "Untitled"
    is added in case of an unsaved rundex.

--- scripts/Interface/ui/drill/presenter/DrillPresenter.py
+++ scripts/Interface/ui/drill/presenter/DrillPresenter.py
@@ -45,6 +45,8 @@ class DrillPresenter:
         """
         self.model = DrillModel()
         self.view = view
+        self._windowTitle = self.view.windowTitle()
+        self.view.setWindowTitle("Untitled [*] -- " + self._windowTitle)
         self._invalidCells = set()
         self._processError = set()
         self._customOptions = set()
@@ -421,6 +423,7 @@ class DrillPresenter:
         if not filename[0]:
             return
         self.model.setIOFile(filename[0])
+        self.view.setWindowTitle(filename[0] + "[*] -- " + self._windowTitle)
         self.model.importRundexData()
         self._syncViewHeader()
         self._syncViewTable()
@@ -449,6 +452,7 @@ class DrillPresenter:
         if not filename[0]:
             return
         self.model.setIOFile(filename[0])
+        self.view.setWindowTitle(filename[0] + "[*] -- " + self._windowTitle)
         self.model.setVisualSettings(self.view.getVisualSettings())
         self.model.exportRundexData()
         self.view.setWindowModified(False)
@@ -510,6 +514,7 @@ class DrillPresenter:
             self._saveDataQuestion()
         self.model.clear()
         self.model.resetIOFile()
+        self.view.setWindowTitle("Untitled [*] -- " + self._windowTitle)
         self._syncViewHeader()
         self._syncViewTable()
 

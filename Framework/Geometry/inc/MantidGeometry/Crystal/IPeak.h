73700e4be5f18e9ad601521fc2ac4fd2392249fe
Samuel Jones <samuel.jones@stfc.ac.uk>
2021-03-26 14:24:32 +0000

    Clang format 11 + config changes
    Update rebased clang-format
    
    Allow multiple documents in pre-commit framework
    
    Add clang-format jenkins job check, to rely on pre-commit
    
    Edit job for clang-format to exit on find of pre-commit clang-format
    
    Fix typo in clang-format script
    
    Check parts of line instead of full line clang-format jenkins

--- Framework/Geometry/inc/MantidGeometry/Crystal/IPeak.h
+++ Framework/Geometry/inc/MantidGeometry/Crystal/IPeak.h
@@ -27,8 +27,7 @@ class MANTID_GEOMETRY_DLL IPeak {
 public:
   virtual ~IPeak() = default;
   virtual Geometry::IDetector_const_sptr getDetector() const = 0;
-  virtual std::shared_ptr<const Geometry::ReferenceFrame>
-  getReferenceFrame() const = 0;
+  virtual std::shared_ptr<const Geometry::ReferenceFrame> getReferenceFrame() const = 0;
 
   virtual int getRunNumber() const = 0;
   virtual void setRunNumber(int m_RunNumber) = 0;
@@ -56,10 +55,8 @@ public:
 
   virtual Mantid::Kernel::V3D getQLabFrame() const = 0;
   virtual Mantid::Kernel::V3D getQSampleFrame() const = 0;
-  virtual void setQSampleFrame(const Mantid::Kernel::V3D &QSampleFrame,
-                               boost::optional<double> detectorDistance) = 0;
-  virtual void setQLabFrame(const Mantid::Kernel::V3D &QLabFrame,
-                            boost::optional<double> detectorDistance) = 0;
+  virtual void setQSampleFrame(const Mantid::Kernel::V3D &QSampleFrame, boost::optional<double> detectorDistance) = 0;
+  virtual void setQLabFrame(const Mantid::Kernel::V3D &QLabFrame, boost::optional<double> detectorDistance) = 0;
 
   virtual void setWavelength(double wavelength) = 0;
   virtual double getWavelength() const = 0;
@@ -90,8 +87,7 @@ public:
   virtual void setIntMNP(const Mantid::Kernel::V3D &MNP) = 0;
 
   virtual Mantid::Kernel::Matrix<double> getGoniometerMatrix() const = 0;
-  virtual void setGoniometerMatrix(
-      const Mantid::Kernel::Matrix<double> &m_GoniometerMatrix) = 0;
+  virtual void setGoniometerMatrix(const Mantid::Kernel::Matrix<double> &m_GoniometerMatrix) = 0;
 
   virtual std::string getBankName() const = 0;
   virtual int getRow() const = 0;
